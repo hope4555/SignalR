a94f73d2a72655102ce15543960e393c10f15274
Andrew Stanton-Nurse <andrew@andrewnurse.net>
2017-02-22 15:02:51 -0800

    stash

--- src/Microsoft.AspNetCore.Sockets.Common/Formatters/MessageFormatter.cs
+++ src/Microsoft.AspNetCore.Sockets.Common/Formatters/MessageFormatter.cs
@@ -7,6 +7,12 @@ namespace Microsoft.AspNetCore.Sockets.Formatters
 {
     public static class MessageFormatter
     {
+        public static readonly byte TextFormatIndicator = (byte)'T';
+        public static readonly byte BinaryFormatIndicator = (byte)'B';
+
+        public static readonly string TextContentType = "application/vnd.microsoft.aspnet.endpoint-messages.v1+text";
+        public static readonly string BinaryContentType = "application/vnd.microsoft.aspnet.endpoint-messages.v1+binary";
+
         public static bool TryFormatMessage(Message message, Span<byte> buffer, MessageFormat format, out int bytesWritten)
         {
             if (!message.EndOfMessage)
@@ -27,6 +33,26 @@ namespace Microsoft.AspNetCore.Sockets.Formatters
             return format == MessageFormat.Text ?
                 TextMessageFormatter.TryParseMessage(buffer, out message, out bytesConsumed) :
                 BinaryMessageFormatter.TryParseMessage(buffer, out message, out bytesConsumed);
+        }
+
+        public static string GetContentType(MessageFormat messageFormat)
+        {
+            switch (messageFormat)
+            {
+                case MessageFormat.Text: return TextContentType;
+                case MessageFormat.Binary: return BinaryContentType;
+                default: throw new InvalidOperationException($"Invalid message format: {messageFormat}");
+            }
+        }
+
+        public static byte GetFormatIndicator(MessageFormat messageFormat)
+        {
+            switch (messageFormat)
+            {
+                case MessageFormat.Text: return TextFormatIndicator;
+                case MessageFormat.Binary: return BinaryFormatIndicator;
+                default: throw new InvalidOperationException($"Invalid message format: {messageFormat}");
+            }
         }
     }
 }
